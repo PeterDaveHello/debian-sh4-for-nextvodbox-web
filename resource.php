66b2757a85324b13c09b9037d33c38b944921f22
Peter Dave Hello <hsu@peterdavehello.org>
2013-10-12 14:56:06 +0800

    update resource list

--- resource.php
+++ resource.php
@@ -1,6 +1,6 @@
 <? require_once("header.php");?>
 
-  <p class="text-info">
+  <p class="text-primary">
     改機以及使用、開發相關資源：
     <ul>
       <li>Debian for 網樂通 <a href="https://sites.google.com/site/debiansh4/home">https://sites.google.com/site/debiansh4/home</a></li>
@@ -9,11 +9,20 @@
       <li>網樂通改機俱樂部(Facebook社團) <a href="https://www.facebook.com/groups/477359555618518/">https://www.facebook.com/groups/477359555618518/</a></li>
       <li>NextVOD unit Hacking - 網樂通改機 <a href="http://www.osslab.com.tw/index.php?title=Hardware/Open_Embedded_System/NextVOD_unit_Hacking_-_%E7%B6%B2%E6%A8%82%E9%80%9A%E6%94%B9%E6%A9%9F">http://nextpbx.osslab.com.tw/</a></li>
       <li>批踢踢實業坊(PTT) <a href="http://www.ptt.cc/man/NextTV/index.html">http://www.ptt.cc/man/NextTV/index.html</a></li>
-      <li>壹電視「網樂通」拆解、改BT機(T客幫) <a href="http://www.techbang.com/posts/11554-dead-screen-bt-letong-hard-modification-machine-hard-circuit-modification-was-put-up-on-bulk-usb-pen-drive-king-of-computers-100-battle-team">http://ppt.cc/jgLt</a></li>
+      <li>壹電視「網樂通」拆解、改BT機 硬改電路掛大容量隨身碟(T客幫) <a href="http://www.techbang.com/posts/11554-dead-screen-bt-letong-hard-modification-machine-hard-circuit-modification-was-put-up-on-bulk-usb-pen-drive-king-of-computers-100-battle-team">http://ppt.cc/jgLt</a><br />(注意:沒有電子硬體相關基礎切勿嘗試硬改以免損壞,另有網友指出文中照片有誤,照抄會出事...)</li>
       <li>T客幫 - 網樂通相關文章<a href="http://www.techbang.com/tags/5707">http://www.techbang.com/tags/5707</a></li>
     </ul>
   </p>
   <p class="text-success">
+    Linux 相關學習資源：
+    <ul>
+      <li>Debian說明文件 <a href="http://www.debian.org/doc/">http://www.debian.org/doc/</a></li>
+      <li>Debian - 維基教科書 <a href="http://zh.wikibooks.org/wiki/Debian">http://zh.wikibooks.org/wiki/Debian</a></li>
+      <li>Ubuntu 正體中文 Wiki <a href="http://wiki.ubuntu-tw.org/index.php">http://wiki.ubuntu-tw.org/index.php</a></li>
+      <li>鳥哥的 Linux 私房菜(偏RedHat系列,但大同小異) <a href="http://linux.vbird.org/">http://linux.vbird.org/</a></li>
+    </ul>
+  </p>
+  <p class="text-info">
     參考資料
     <ul>
       <li>網樂通終止服務新聞(蘋果日報)<a href="http://www.appledaily.com.tw/realtimenews/article/life/20121001/145055">http://www.appledaily.com.tw/realtimenews/article/life/20121001/145055</a></li>
