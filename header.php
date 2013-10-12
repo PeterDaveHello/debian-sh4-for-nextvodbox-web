cef817fc13f8e6d6e87440d68612c3b1bf1a0c81
Peter Dave Hello <hsu@peterdavehello.org>
2013-10-12 12:36:26 +0800

    add navbar in header

--- header.php
+++ header.php
@@ -18,4 +18,19 @@ h2{color:#FFFF00;}
 <body>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+<div class="navbar-header">
+<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+<span class="sr-only">選單切換</span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+</button>
+<a class="navbar-brand" href="#">Debian-sh4 for NextVODBox</a>
+</div>
 
+<div class="collapse navbar-collapse navbar-ex1-collapse">
+<ul class="nav navbar-nav">
+</ul>
+</nav>
+<br />
