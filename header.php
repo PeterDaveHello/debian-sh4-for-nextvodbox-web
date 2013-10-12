be9c227377ed89cfc8560a3e451fd2eaf0efa22d
Peter Dave Hello <hsu@peterdavehello.org>
2013-10-12 12:41:04 +0800

    coding style

--- header.php
+++ header.php
@@ -1,36 +1,34 @@
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="utf-8">
-<title>
-Debian Wheezy sh4 for 網樂通(NextVODBox)
-</title>
-<meta name="description" content="網樂通用的Debian! Unofficial Debian Wheezy SH4 for Nextvodbox" />
-<meta name="keywords" content="網樂通, nextvodbox, wheezy, sh4, OS, Debian, Linux, RISC, Download" />
-<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
-<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-theme.min.css" rel="stylesheet">
-<style>
-body {background-color:#4D4D4D;font-size:xx-large;color:#FF0066;}
-a, a:hover{color:#000066}
-h2{color:#FFFF00;}
-</style>
-</head>
-<body>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-<div class="navbar-header">
-<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-<span class="sr-only">選單切換</span>
-<span class="icon-bar"></span>
-<span class="icon-bar"></span>
-<span class="icon-bar"></span>
-</button>
-<a class="navbar-brand" href="#">Debian-sh4 for NextVODBox</a>
-</div>
+  <head>
+    <meta charset="utf-8">
+    <title>Debian Wheezy sh4 for 網樂通(NextVODBox)</title>
+    <meta name="description" content="網樂通用的Debian! Unofficial Debian Wheezy SH4 for Nextvodbox" />
+    <meta name="keywords" content="網樂通, nextvodbox, wheezy, sh4, OS, Debian, Linux, RISC, Download" />
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-theme.min.css" rel="stylesheet" />
+    <style>
+      body {background-color:#4D4D4D; font-size:xx-large; color:#FF0066;}
+      a, a:hover {color:#000066;}
+      h2 {color:#FFFF00;}
+    </style>
+  </head>
+  <body>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+          <span class="sr-only">選單切換</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Debian-sh4 for NextVODBox</a>
+      </div>
 
-<div class="collapse navbar-collapse navbar-ex1-collapse">
-<ul class="nav navbar-nav">
-</ul>
-</nav>
-<br />
+      <div class="collapse navbar-collapse navbar-ex1-collapse">
+        <ul class="nav navbar-nav">
+      </ul>
+    </nav>
+    <br />
