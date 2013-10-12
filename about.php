c3c8b5610a96cfd0fe8c7db6f8db22cd3b73c940
Peter Dave Hello <hsu@peterdavehello.org>
2013-10-12 14:20:46 +0800

    add about page

---
+++ about.php
@@ -0,0 +1,26 @@
+<? require_once("header.php");?>
+
+  <p class="text-info">
+    改機以及使用、開發相關資源：
+    <ul>
+      <li>Debian for 網樂通 <a href="https://sites.google.com/site/debiansh4/home">https://sites.google.com/site/debiansh4/home</a></li>
+      <li>sh4twbox <a href="https://code.google.com/p/sh4twbox/">https://code.google.com/p/sh4twbox/</a></li>
+      <li>網樂通改機俱樂部(獨立論壇) <a href="http://next.fishome.tw/bbs/">http://next.fishome.tw/bbs/</a></li>
+      <li>網樂通改機俱樂部(Facebook社團) <a href="https://www.facebook.com/groups/477359555618518/">https://www.facebook.com/groups/477359555618518/</a></li>
+      <li>NextVOD unit Hacking - 網樂通改機 <a href="http://www.osslab.com.tw/index.php?title=Hardware/Open_Embedded_System/NextVOD_unit_Hacking_-_%E7%B6%B2%E6%A8%82%E9%80%9A%E6%94%B9%E6%A9%9F">http://nextpbx.osslab.com.tw/</a></li>
+      <li>批踢踢實業坊(PTT) <a href="http://www.ptt.cc/man/NextTV/index.html">http://www.ptt.cc/man/NextTV/index.html</a></li>
+      <li>壹電視「網樂通」拆解、改BT機(T客幫) <a href="http://www.techbang.com/posts/11554-dead-screen-bt-letong-hard-modification-machine-hard-circuit-modification-was-put-up-on-bulk-usb-pen-drive-king-of-computers-100-battle-team">http://ppt.cc/jgLt</a></li>
+      <li>T客幫 - 網樂通相關文章<a href="http://www.techbang.com/tags/5707">http://www.techbang.com/tags/5707</a></li>
+    </ul>
+  </p>
+  <p class="text-success">
+    參考資料
+    <ul>
+      <li>網樂通終止服務新聞(蘋果日報)<a href="http://www.appledaily.com.tw/realtimenews/article/life/20121001/145055">http://www.appledaily.com.tw/realtimenews/article/life/20121001/145055</a></li>
+      <li>網樂通終止服務新聞(ETToday)<a href="http://www.ettoday.net/news/20121001/109377.htm">http://www.ettoday.net/news/20121001/109377.htm</a></li>
+      <li>壹網樂 - 維基百科 <a href="http://zh.wikipedia.org/wiki/%E5%A3%B9%E7%B6%B2%E6%A8%82">http://zh.wikipedia.org/wiki/%E5%A3%B9%E7%B6%B2%E6%A8%82</a></li>
+      <li>網樂通 - 維基百科 <a href="http://zh.wikipedia.org/wiki/%E7%B6%B2%E6%A8%82%E9%80%9A">http://zh.wikipedia.org/wiki/%E7%B6%B2%E6%A8%82%E9%80%9A</a></li>
+    </ul>
+  </p>
+
+<? require_once("footer.php");?>
