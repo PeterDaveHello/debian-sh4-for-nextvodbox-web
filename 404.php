7442b1e197abb3706cf171a26e9370c1d7704140
Peter Dave Hello <hsu@peterdavehello.org>
2013-10-12 14:58:34 +0800

    update 404 page

--- 404.php
+++ 404.php
@@ -1,7 +1,7 @@
 <? require_once("header.php");?>
   <center>
     您要找的頁面不存在 - 404 Error<br />
-    <hr />
+    可能是本頁面正在製作/修改中，或是您輸入了一個不存在的路徑<br />
     <iframe src="http://404page.missingkids.org.tw/api?key=xsPrYr53z4JS1hyXonJy" width="100%" height="635" frameborder="0"></iframe>
   </center>
 <? require_once("footer.php");?>
