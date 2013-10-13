74ac0b49a215426c2cbe4632f0398a9513fec177
Peter Dave Hello <hsu@peterdavehello.org>
2013-10-13 20:01:29 +0800

    update activity page

--- activity.php
+++ activity.php
@@ -1,5 +1,6 @@
 <? require_once("template/header.php");?>
-
-        <iframe src="https://www.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=9d0i18dr57k7gj4qn1n3pkraog%40group.calendar.google.com&amp;color=%232952A3&amp;ctz=Asia%2FTaipei" style=" border-width:0 " width="100%" height="600px" frameborder="0" scrolling="no"></iframe>
-
+        <p class="text-primary">
+          網樂通開發者/使用者聚會或是開發重大記事/里程碑將為在未來列入日曆中供大家參考。
+          <iframe src="https://www.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=9d0i18dr57k7gj4qn1n3pkraog%40group.calendar.google.com&amp;color=%232952A3&amp;ctz=Asia%2FTaipei" style=" border-width:0 " width="100%" height="600px" frameborder="0" scrolling="no"></iframe>
+        </p>
 <? require_once("template/footer.php");?>
