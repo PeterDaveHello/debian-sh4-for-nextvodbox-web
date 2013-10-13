a6fcc3bd7b34baa89d8541952a46fd32cd5754f9
Peter Dave Hello <hsu@peterdavehello.org>
2013-10-13 19:59:59 +0800

    add sponsor page

---
+++ sponsor.php
@@ -0,0 +1,7 @@
+<? require_once("template/header.php");?>
+
+    <p class="text-primary">
+      基於開發/測試用途, 我們非常需要您手上多餘/閒置的網樂通機上盒, 歡迎願意贊助網樂通機上盒的朋友跟我們聯絡。
+    </p>
+
+<? require_once("template/footer.php");?>
