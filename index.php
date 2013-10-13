65ed7acc7b973ecc01b91b3ea2c8e3eaa088071b
Peter Dave Hello <hsu@peterdavehello.org>
2013-10-14 00:25:17 +0800

    update index

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <? require_once("template/header.php");?>
 
         <p class="lead text-primary">
-          網樂通機上盒本來是壹電視免費借給台灣用戶的網路電視數位機上盒，透過網路，可以隨選隨看各樣的電影或是收聽電台，並且可以觀賞部份高畫質頻道，如：壹電視新聞台等，因為壹傳媒主席黎智英決定退出台灣市場後而停止服務，而壹傳媒也很大方的把網樂通無償贈與當初申請租借的使用者，自此後相繼有高手投入開發，希望能解放網樂通剩下的價值，從而有sh4twbox, NextPBX, Wonderbox等應用。
+          <a href="http://zh.wikipedia.org/zh-tw/%E7%B6%B2%E6%A8%82%E9%80%9A">網樂通</a>機上盒本來是壹電視免費借給台灣用戶的網路電視數位機上盒，透過網路，可以隨選隨看各樣的電影或是收聽電台，並且可以觀賞部份高畫質頻道，如：壹電視新聞台等，因為壹傳媒主席黎智英決定退出台灣市場後而停止服務，而壹傳媒也很大方的把網樂通無償贈與當初申請租借的使用者，自此後相繼有高手投入開發，希望能解放網樂通剩下的價值，從而有<a href="https://code.google.com/p/sh4twbox/">sh4twbox</a>, <a href="http://nextpbx.osslab.com.tw">NextPBX</a>, <a href="http://www.wonderbox.net.tw/tw/nasbox.html">Wonderbox</a>等應用。
         </p>
         <p class="text-info">
           本專案由"<strong>網樂通開發/研究/貢獻/準開發者</strong>"社團發起，希望能在網樂通上面移植一套完整的Debian GNU/Linux作業系統，讓網樂通能有更好的發揮，也讓新手更容易加入與學習。
+        </p>
+        <hr />
+        <hr />
+        <hr />
+        <hr />
+        <p class="text-muted">
+          網站目前處於建制初期，內容會陸續補齊，如果您有任何建議或是興趣加入內容的撰寫維護歡迎聯絡我們。
         </p>
 
 <? require_once("template/footer.php");?>
