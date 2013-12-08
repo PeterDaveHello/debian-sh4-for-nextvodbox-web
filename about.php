6e81e086a03c6b8d109e6df95c40442d5064e1be
Peter Dave Hello <hsu@peterdavehello.org>
2013-12-08 17:37:09 +0800

    update about page

--- about.php
+++ about.php
@@ -1,8 +1,9 @@
 <? require_once("template/header.php");?>
 
         <p class="text-primary">
-          因為開發以及維護都是大家自發，並非專職從事本專案，目前人手不足，無暇製作出精美的意見表格，若有需要聯絡可以發送電子郵件到這個信箱，謝謝!<br />
-          <img src="mail.png">
+          因為開發以及維護都是大家自發，並非專職從事本專案，目前人手不足，無暇製作出精美的意見表格，若有需要聯絡可以發送電子郵件到這個信箱<img src="mail.png">
+          <hr />
+          若是使用上的問題討論相關建議至<a href="https://www.facebook.com/groups/477359555618518/">『網樂通改機俱樂部』Facebook社團</a>，謝謝！
         </p>
 
 <? require_once("template/footer.php");?>
