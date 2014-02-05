20803013c029aae8253e0812d7834faae4b53620
Peter Dave Hello <hsu@peterdavehello.org>
2014-02-05 19:31:08 +0800

    update about page

--- about.php
+++ about.php
@@ -3,6 +3,8 @@
         <p class="text-primary">
           因為開發以及維護都是大家自發，並非專職從事本專案，目前人手不足，無暇製作出精美的意見表格，若有需要聯絡可以發送電子郵件到這個信箱<img src="mail.png">
           <hr />
+          如果有不慎提供錯誤資訊或是錯字等問題，歡迎上<a href="https://github.com/PeterDaveHello/debian-sh4-for-nextvodbox-web">github</a>回報，更歡迎您協助動手修改
+          <hr />
           若是使用上的問題討論相關建議至<a href="https://www.facebook.com/groups/477359555618518/">『網樂通改機俱樂部』Facebook社團</a>，謝謝！
         </p>
 
