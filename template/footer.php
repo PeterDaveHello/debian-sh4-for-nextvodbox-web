eef7766d5898aea1809c7ccbc7e10211a68a7c0f
Peter Dave Hello <hsu@peterdavehello.org>
2015-03-24 14:54:08 +0800

    update footer

--- template/footer.php
+++ template/footer.php
@@ -1,7 +1,6 @@
         </div>
       </div>
     </div>
-    <span class="pull-right" id="hosting">Hosting By <a href="http://forum.cse.yzu.edu.tw">元智人論壇</a></span>
     <br />
     <br />
   </body>
